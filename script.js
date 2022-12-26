309aafde436d4d905c8f6b7578e2469e064edcb9
muhammad-ehtisham-haider <120640715+muhammad-ehtisham-haider@users.noreply.github.com>
2022-12-26 09:43:32 +0500

    change
    this design is only for desktop not for mobile

---
+++ script.js
@@ -0,0 +1,107 @@
+var rotation = 0;
+function rotateImage() {
+  rotation += 180; 
+  if (rotation === 360) { 
+
+    rotation = 0;
+  }
+  document.querySelector("#arrowOne").style.transform = `rotate(${rotation}deg)`;
+
+}
+
+function setProperty() {
+    var x = document.getElementById("ans-one") ;
+    if (x.style.display === "none") {
+        x.style.display = "block" ;
+    } else {
+        x.style.display = "none" ;
+    }
+}
+
+
+
+var rotation = 0;
+function rotateImageTwo() {
+  rotation += 180; 
+  if (rotation === 360) { 
+
+    rotation = 0;
+  }
+  document.querySelector("#arrowTwo").style.transform = `rotate(${rotation}deg)`;
+}
+
+
+function setPropertytwo() {
+    var x = document.getElementById("ans-two") ;
+    if (x.style.display === "none") {
+        x.style.display = "block" ;
+    } else {
+        x.style.display = "none" ;
+    }
+}
+
+var rotation = 0;
+function rotateImageThree() {
+  rotation += 180; 
+  if (rotation === 360) { 
+
+    rotation = 0;
+  }
+  document.querySelector("#arrowThree").style.transform = `rotate(${rotation}deg)`;
+}
+
+
+function setPropertythree() {
+    var x = document.getElementById("ans-three") ;
+    if (x.style.display === "none") {
+        x.style.display = "block" ;
+    } else {
+        x.style.display = "none" ;
+    }
+}
+
+
+
+var rotation = 0;
+function rotateImagefour() {
+  rotation += 180; 
+  if (rotation === 360) { 
+
+    rotation = 0;
+  }
+  document.querySelector("#arrowFour").style.transform = `rotate(${rotation}deg)`;
+}
+
+
+function setPropertyfour() {
+    var x = document.getElementById("ans-four") ;
+    if (x.style.display === "none") {
+        x.style.display = "block" ;
+    } else {
+        x.style.display = "none" ;
+    }
+}
+
+var rotation = 0;
+function rotateImagefive() {
+  rotation += 180; 
+  if (rotation === 360) { 
+
+    rotation = 0;
+  }
+  document.querySelector("#arrowFive").style.transform = `rotate(${rotation}deg)`;
+}
+
+
+function setPropertyfive() {
+    var x = document.getElementById("ans-five") ;
+    if (x.style.display === "none") {
+        x.style.display = "block" ;
+    } else {
+        x.style.display = "none" ;
+    }
+}
+
+
+
+
